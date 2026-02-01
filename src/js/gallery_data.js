b3a785f671bd50299146dbd500b9f5883fd01937
shreehitha <shreehitha.p@gmail.com>
2026-02-01 14:19:37 +0530

    new images

--- src/js/gallery_data.js
+++ src/js/gallery_data.js
@@ -3,6 +3,34 @@
 
 const galleryImages = [
     {
+        "filename": "01.09.2025 17.16.02 116 315.jpg",
+        "title": "01.09.2025 17.16.02 116 315"
+    },
+    {
+        "filename": "02.08.2025 17.48.07 870 5.jpg",
+        "title": "02.08.2025 17.48.07 870 5"
+    },
+    {
+        "filename": "02.08.2025 17.48.52 578 10.jpg",
+        "title": "02.08.2025 17.48.52 578 10"
+    },
+    {
+        "filename": "20240630_162958.jpg",
+        "title": "20240630 162958"
+    },
+    {
+        "filename": "20241115_173252 (2).jpg",
+        "title": "20241115 173252 (2)"
+    },
+    {
+        "filename": "20241115_174103.jpg",
+        "title": "20241115 174103"
+    },
+    {
+        "filename": "20250830_184511.jpg",
+        "title": "20250830 184511"
+    },
+    {
         "filename": "cogni-2.jpg",
         "title": "Cogni 2"
     },
@@ -37,5 +65,73 @@ const galleryImages = [
     {
         "filename": "IMG-20230616-WA0133.jpg",
         "title": "Img 20230616 Wa0133"
+    },
+    {
+        "filename": "IMG-20250107-WA0041.jpg",
+        "title": "Img 20250107 Wa0041"
+    },
+    {
+        "filename": "IMG-20250402-WA0006.jpg",
+        "title": "Img 20250402 Wa0006"
+    },
+    {
+        "filename": "IMG-20251019-WA0062.jpg",
+        "title": "Img 20251019 Wa0062"
+    },
+    {
+        "filename": "IMG_20260125_193257_163.jpg",
+        "title": "Img 20260125 193257 163"
+    },
+    {
+        "filename": "PSX_20241005_011519.jpg",
+        "title": "Psx 20241005 011519"
+    },
+    {
+        "filename": "PSX_20241005_012026.jpg",
+        "title": "Psx 20241005 012026"
+    },
+    {
+        "filename": "PSX_20241006_020806.jpg",
+        "title": "Psx 20241006 020806"
+    },
+    {
+        "filename": "PSX_20241019_164659.jpg",
+        "title": "Psx 20241019 164659"
+    },
+    {
+        "filename": "PSX_20241115_193241.jpg",
+        "title": "Psx 20241115 193241"
+    },
+    {
+        "filename": "PSX_20241223_013205.jpg",
+        "title": "Psx 20241223 013205"
+    },
+    {
+        "filename": "PSX_20241223_021740.jpg",
+        "title": "Psx 20241223 021740"
+    },
+    {
+        "filename": "PSX_20241223_022442.jpg",
+        "title": "Psx 20241223 022442"
+    },
+    {
+        "filename": "PSX_20250626_004352.jpg",
+        "title": "Psx 20250626 004352"
+    },
+    {
+        "filename": "PSX_20250706_210155.jpg",
+        "title": "Psx 20250706 210155"
+    },
+    {
+        "filename": "PSX_20250707_001943.jpg",
+        "title": "Psx 20250707 001943"
+    },
+    {
+        "filename": "PSX_20250802_172235.jpg",
+        "title": "Psx 20250802 172235"
+    },
+    {
+        "filename": "sn-0184-01 (13).jpg",
+        "title": "Sn 0184 01 (13)"
     }
 ];
